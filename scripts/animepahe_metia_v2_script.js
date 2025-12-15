65589d204e90f32090fd6779b2eec9b01c5cf4f7
Emdy1 <mdhzair1@gmail.com>
2025-12-15 21:29:14 +0100

    iniiiit

--- scripts/animepahe_metia_v2_script.js
+++ scripts/animepahe_metia_v2_script.js
@@ -1,4 +1,5 @@
 async function searchAnime(keyword) {
+    log("searching for " + keyword + ".");
     const headers = {
         'Cookie': '__ddg2_=',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
